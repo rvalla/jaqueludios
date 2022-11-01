48cdef985add8841aa248f9a31aba868e423ee12
Rodrigo Valla <rodrigovalla@protonmail.ch>
2022-10-31 23:00:08 -0300

    Correcting error on GameImages()...

--- tools/game_to_imgs.py
+++ tools/game_to_imgs.py
@@ -15,7 +15,7 @@ class GameImages():
 		self.size = self.config["size"]
 		self.game = chpgn.read_game(open(self.config["game_path"]))
 		self.pieces = self.load_pieces()
-		self.board_state = self.start_board()
+		self.board = ch.Board()
 		if not os.path.exists(self.config["output_path"]):
 			os.mkdir(self.config["output_path"])
 		self.game_to_images()
@@ -26,24 +26,23 @@ class GameImages():
 		self.save_board_state(0)
 		for move in self.game.mainline_moves():
 			print("Working on move " + str(m), end="\r")
-			self.make_move(move)
+			self.board.push(move)
 			m += 1
 			if m % self.period == 0:
 				self.save_board_state(m // self.period)
 
-	def make_move(self, move):
-		origin = move.from_square
-		destination = move.to_square
-		piece_at = self.board_state[origin]
-		self.board_state[origin] = None
-		self.board_state[destination] = piece_at
-
 	def save_board_state(self, image_n):
 		image = im.open(self.config["background"]).resize((self.size, self.size))
 		for sq in range(64):
-			if not self.board_state[sq] == None:
+			piece = self.board.piece_at(sq)
+			if not piece == None:
 				f, c = divmod(sq,8)
-				piece_image = self.pieces[self.board_state[sq][0]][self.board_state[sq][1]-1]
+				color = None
+				if piece.color:
+					color = 0
+				else:
+					color = 1
+				piece_image = self.pieces[color][piece.piece_type-1]
 				image.paste(piece_image, (c * self.sq_size, (7-f) * self.sq_size), mask=piece_image)
 		name = self.config["output_name"] + "_" + ("{:03d}").format(image_n) + ".png"
 		image.save(self.config["output_path"] + "/" + name, "png", quality=85, optimize=True)
@@ -68,12 +67,6 @@ class GameImages():
 		pieces.append(white)
 		pieces.append(black)
 		return pieces
-
-	def start_board(self):
-		squares = [None for sq in range(64)]
-		squares[0:15] = [(0,4), (0,2), (0,3), (0,5), (0,6), (0,3), (0,2), (0,4), (0,1), (0,1), (0,1), (0,1), (0,1), (0,1), (0,1), (0,1)]
-		squares[48:63] = [(1,1), (1,1), (1,1), (1,1), (1,1), (1,1), (1,1), (1,1), (1,4), (1,2), (1,3), (1,5), (1,6), (1,3), (1,2), (1,4)]
-		return squares
 
 	def __str__(self):
 		return "Hi, I am an the class to transform a chess game on a serieas of images..."
