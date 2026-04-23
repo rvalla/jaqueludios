74aac133392525b3e329c1fc611bb35d3f2d3fc2
Rodrigo Valla <rodrigovalla@protonmail.ch>
2026-04-23 09:28:06 -0300

    New games to images...

--- tools/game_to_imgs.py
+++ tools/game_to_imgs.py
@@ -1,5 +1,5 @@
 import os
-import json as js
+import random as rd
 from PIL import Image as im
 import chess as ch
 import chess.pgn as chpgn
@@ -7,17 +7,19 @@ import chess.pgn as chpgn
 class GameImages():
 	"The class to convert a chess game in a series of images"
 
-	def __init__(self, config_path):
+	def __init__(self, period, sq_size, size, game_path, output_path, file_name):
 		print("Starting new series of images...", end="\n")
-		self.config = js.load(open(config_path))
-		self.period = self.config["period"]
-		self.sq_size = self.config["sq_size"]
-		self.size = self.config["size"]
-		self.game = chpgn.read_game(open(self.config["game_path"]))
+		self.period = period
+		self.sq_size = sq_size
+		self.size = size
+		self.game = chpgn.read_game(open(game_path))
 		self.pieces = self.load_pieces()
+		self.output_path = output_path
+		self.file_name = file_name
 		self.board = ch.Board()
-		if not os.path.exists(self.config["output_path"]):
-			os.mkdir(self.config["output_path"])
+		self.background = rd.choice(["img/boardR.png", "img/boardG.png", "img/boardB.png"])
+		if not os.path.exists(output_path):
+			os.mkdir(output_path)
 		self.game_to_images()
 		print("That's all!            ", end="\n")
 
@@ -32,7 +34,7 @@ class GameImages():
 				self.save_board_state(m // self.period)
 
 	def save_board_state(self, image_n):
-		image = im.open(self.config["background"]).resize((self.size, self.size))
+		image = im.open(self.background).resize((self.size, self.size))
 		for sq in range(64):
 			piece = self.board.piece_at(sq)
 			if not piece == None:
@@ -44,8 +46,8 @@ class GameImages():
 					color = 1
 				piece_image = self.pieces[color][piece.piece_type-1]
 				image.paste(piece_image, (c * self.sq_size, (7-f) * self.sq_size), mask=piece_image)
-		name = self.config["output_name"] + "_" + ("{:03d}").format(image_n) + ".png"
-		image.save(self.config["output_path"] + "/" + name, "png", quality=85, optimize=True)
+		name = self.file_name + "_" + ("{:03d}").format(image_n) + ".png"
+		image.save(self.output_path + "/" + name, "png", quality=85, optimize=True)
 
 	def load_pieces(self):
 		print("Loading pieces...", end="\r")
